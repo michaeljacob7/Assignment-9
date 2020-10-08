b5a13e4bb8c34616709b187fd62f3bdec8769c2d
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-08 23:25:35 +0530

    Employee Attendance UC1 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+isPresent=1
+randomNumber=$((RANDOM%2))
+
+#TO CHECK THE EMPLOYEE PRESENT AND ABSENT
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
+
 
