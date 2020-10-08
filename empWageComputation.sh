c5f42ddfa5c0ca18e901e9a1c93f3840d72f8b46
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-09 01:51:05 +0530

    Employee Switch Case UC4 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -17,14 +17,29 @@ isPartTime=1;
 isFullTime=2;
 empRatePerHr=100;
 randomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-        empHrs=16;
-elif [ $isPartTime -eq $randomCheck ];
-then
-        empHrs=8;
-else
-        empHrs=0;
-fi
-salary=$(($empHrs*$empRatePerHr));
+#if [ $isFullTime -eq $randomCheck ];
+#then
+#       empHrs=16;
+#elif [ $isPartTime -eq $randomCheck ];
+#then
+#        empHrs=8;
+#else
+#        empHrs=0;
+#fi
 
+#TO USE SWITCH CASE
+function getWorkingHours() {
+        case $1 in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+}
+getWorkingHours $((RANDOM%3))
+salary=$(($empHrs*$empRatePerHr));
