979a5fadf7c2c5c370c049d9896661e0041c310e
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-12 18:17:28 +0530

    UC-6 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,7 +4,6 @@ echo "Welcome to Employee Wage Computation Program"
 isPresent=1
 randomNumber=$((RANDOM%2))
 
-#TO CHECK THE EMPLOYEE PRESENT AND ABSENT
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
@@ -12,25 +11,9 @@ else
 	echo "Employee Absent"
 fi
 
-#TO CHECK DAILY WAGE
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=100;
-#randomCheck=$((RANDOM%3));
-#if [ $isFullTime -eq $randomCheck ];
-#then
-#       empHrs=16;
-#elif [ $isPartTime -eq $randomCheck ];
-#then
-#        empHrs=8;
-#else
-#        empHrs=0;
-#fi
-
-#TO USE SWITCH CASE
-
-
-#ADDING TOTAL WORKING DAYS
 numWorkingDays=20;
 totalWorkHours=0;
 totalWorkingDays=0;
